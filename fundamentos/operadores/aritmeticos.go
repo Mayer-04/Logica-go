1ecc2ec653e259ec8402c7a33898c4a71dc6a9fa
Mayer Andres <andres.20.chaves@gmail.com>
2024-03-02 16:25:55 -0500

    Operadores aritmeticos

---
+++ fundamentos/operadores/aritmeticos.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// TODO: Primero se ejecutan las operaciones entre (), *, /, %, +, -
+	// * Operadores aritmeticos
+
+	// Suma +
+	suma := 10 + 10
+
+	fmt.Println("Suma:", suma)
+
+	// Resta -
+	resta := 10 - 5
+
+	fmt.Println("Resta:", resta)
+
+	// Multiplicación *
+	multiplicacion := 10 * 5
+
+	fmt.Println("Multiplicación:", multiplicacion)
+
+	// División /
+	division := 10 / 5
+
+	fmt.Println("División:", division)
+
+	// Modulo %
+	modulo := 10 % 5
+
+	fmt.Println("Modulo:", modulo)
+
+	// Ejemplo
+	var ejemplo = (2 + 3) * 2
+
+	fmt.Println("Ejemplo:", ejemplo)
+
+}
