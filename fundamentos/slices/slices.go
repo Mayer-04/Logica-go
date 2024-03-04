12482592290eefd484be09f1aa54e65a57dec43a
Mayer Andres <andres.20.chaves@gmail.com>
2024-03-04 11:52:11 -0500

    Slices

--- fundamentos/slices/slices.go
+++ fundamentos/slices/slices.go
@@ -3,9 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var slice []int
-	slice = append(slice, 10)
-	slice = append(slice, 20)
-	slice = append(slice, 30)
+	// Arreglo
+	var array = [5]int{10, 20, 30, 40, 50}
+
+	// Slices
+	slice := array[0:3]
+
 	fmt.Println(slice)
+
+	// Tamaño o longitud de un slice - len()
+	fmt.Println(len(slice))
+
+	// Capacidad de un slice - cap()
+	fmt.Println(cap(slice))
 }
