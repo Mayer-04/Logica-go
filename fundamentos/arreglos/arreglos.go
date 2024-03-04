672df5e5af4b55d062cb2f2cb16c65a113e35710
Mayer Andres <andres.20.chaves@gmail.com>
2024-03-03 19:27:07 -0500

    Características de los arreglos

--- fundamentos/arreglos/arreglos.go
+++ fundamentos/arreglos/arreglos.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+
+	// Arreglo
 	var array [5]int
 	array[0] = 10
 	array[1] = 20
@@ -10,4 +12,27 @@ func main() {
 	array[3] = 40
 	array[4] = 50
 	fmt.Println(array)
+
+	// Arreglos literales - Se declaran los valores entre corchetes
+	array2 := [3]string{"Mayer", "Andres", "Chaves"}
+
+	fmt.Println(array2)
+
+	// Arreglos multidimensionales
+	array3 := [3][2]int{{10, 20}, {30, 40}, {50, 60}}
+
+	fmt.Println(array3)
+
+	// Arreglo de longitud implícita
+	// TODO: Infiere el tamaño basado en los valores que se le asignen
+	array4 := [...]int{10, 20, 30}
+
+	fmt.Println(array4)
+
+	// Arreglos sin asignación de valores
+	// TODO: Los elementos del arreglo tendrán el valor cero de su tipo de dato correspondientes
+	array5 := [2]bool{}
+
+	fmt.Println(array5)
+
 }
