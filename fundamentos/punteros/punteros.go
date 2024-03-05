99c39b54fb934283737fed9efe11a92b550cf667
Mayer Andres <andres.20.chaves@gmail.com>
2024-03-04 22:45:43 -0500

    Añadiendo punteros

--- fundamentos/punteros/punteros.go
+++ fundamentos/punteros/punteros.go
@@ -2,15 +2,41 @@ package main
 
 import "fmt"
 
+// Función que toma un puntero y modifica el valor apuntado
+func incrementar(puntero *int) {
+	*puntero++
+}
+
 func main() {
 
-	var a int = 10
+	// Declaración de una variable
+	var numero int = 42
+	// Declaración de un puntero con el *
+	var puntero *int
 
-	var b *int = &a
+	// Asignación del puntero
+	puntero = &numero
 
-	fmt.Printf("Tipo: %T - Valor: %v - Puntero: %v", b, b, *b)
+	// Acceso al valor a través del puntero
+	fmt.Println("Valor de numero:", *puntero) // Output: Valor de x: 42
 
-	*b = 20
+	// TODO: Operador desreferenciación *
+	//  Acceder al valor almacenado en la dirección de memoria apuntada por un puntero y modificarlo.
+	*puntero = 10
 
-	fmt.Printf("\nTipo: %T - Valor: %v - Puntero: %v", b, b, *b)
+	fmt.Println("Nuevo valor de numero:", numero) // Output: Nuevo valor de x: 10
+
+	// TODO: Ejemplo dos: Declarando una variable puntero y asignandole la posición en memoria
+	fruit := "🍎"
+	var pointer *string = &fruit
+
+	fmt.Println(pointer)
+
+	// TODO: Ejemplo con la función incrementar
+	var x int = 10
+
+	// Pasar un puntero a la función incrementar
+	incrementar(&x)
+
+	fmt.Printf("Valor de x: %d, Tipo: %T\n", x, x) // Output: Valor de x: 11
 }
