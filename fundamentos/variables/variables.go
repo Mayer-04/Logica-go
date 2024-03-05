e385fa405a02a9c285b327f15205d77faab00dac
Mayer Andres <andres.20.chaves@gmail.com>
2024-03-05 12:42:47 -0500

    Añadiendo nuevas características a las variables

--- fundamentos/variables/variables.go
+++ fundamentos/variables/variables.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func main() {
 
+	// TODO: La declaración de variable corta solo puede usarse dentro del cuerpo de una función.
+
 	// Declarando una variable con su tipo
 	var name string = "Mayer"
 
@@ -20,6 +22,11 @@ func main() {
 
 	fmt.Println("Apellido:", lastname)
 
+	// Definición de múltiples variables.
+	var casa1, casa2, casa3 string = "🏠", "🏡", "🏚️"
+
+	fmt.Println(casa1, casa2, casa3)
+
 	// Declaración de variable corta - Infiere automáticamente el tipo
 	// TODO: No podemos reasignar un valor con la declaración de variable corta.
 	age := 23
@@ -34,4 +41,10 @@ func main() {
 	)
 
 	fmt.Println(home, user, animal)
+
+	// Definición de tres variables de tipo number, y sus valores iniciales.
+	numero1, numero2, numero3 := 2, 3, 4
+
+	fmt.Println(numero1, numero2, numero3)
+
 }
