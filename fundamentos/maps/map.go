a9c54923dd920a359e7fef57d53295985d96689e
Mayer Andres <andres.20.chaves@gmail.com>
2024-03-06 11:39:35 -0500

    Agregando maps y structs

--- fundamentos/maps/map.go
+++ fundamentos/maps/map.go
@@ -3,11 +3,41 @@ package main
 import "fmt"
 
 func main() {
-	var colors map[string]string
-	colors = make(map[string]string)
-	colors["red"] = "#ff0000"
-	colors["green"] = "#00ff00"
-	colors["blue"] = "#0000ff"
-	fmt.Println(colors)
-	fmt.Println(colors["red"])
+
+	// Creando un map literal - declarar y asignar inmediatamente el valor
+	// TODO: Obligatoria la coma al final de cada elemento
+	var edades = map[string]int{
+		"Mayer":  24,
+		"Andres": 23,
+		"Luis":   25,
+		"Maria":  26,
+	}
+
+	fmt.Println("Edades:", edades)
+
+	// Creando e inicializando un map con make
+	edades2 := make(map[string]int)
+
+	edades2["Mayer"] = 24
+	edades2["Andres"] = 23
+
+	fmt.Println("Edades:", edades2)
+
+	// Creando un map con valores iniciales
+	nuevoMapa := map[string]int{"a": 1, "b": 2, "c": 3}
+
+	fmt.Println(nuevoMapa)
+
+	// Accediendo a un elemento del map
+	// TODO: Si no existe la llave, nos devuelve el valor 0 del tipo de dato correspondiente
+	fmt.Println(nuevoMapa["a"])
+	fmt.Println("Llave d:", nuevoMapa["d"]) // Output: Llave d: 0
+
+	// Eliminar un elemento del map a partir de su llave(clave) - delete
+	delete(nuevoMapa, "a")
+
+	// Verificar si un elemento existe en el map
+	content, ok := nuevoMapa["a"]
+
+	fmt.Printf("Contenido: %v - Existe: %t", content, ok)
 }
---
+++ fundamentos/structs/struct.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+// TODO: Struct - Una definición de una clase en Go
+// Suelen declararse a nivel de paquete y no a nivel de funciones
+type Persona struct {
+	Nombre string
+	Edad   uint8
+}
+
+func main() {
+
+	// Creando una variable - Instanciando una persona
+	// TODO: Se debe inicializar todos los campos - RECOMENDADO
+	luis := Persona{
+		Nombre: "Luis",
+		Edad:   25,
+	}
+
+	fmt.Println("Persona1:", luis)
+
+	// Definiendo solo un campo
+	// TODO: Los campos no son obligatorios - Los demás campos tomaran el valor 0 de su tipo por defecto
+	andres := Persona{
+		Nombre: "Andres",
+	}
+
+	fmt.Println("Persona2:", andres)
+
+	// No se define el nombre de los campos pero se respeta el orden
+	mayer := Persona{"Mayer", 23}
+
+	fmt.Println("Persona3:", mayer)
+
+	// Accediendo a los campos - Usando el operador punto
+	fmt.Println(luis.Nombre) // Output: Luis
+	fmt.Println(luis.Edad)   // Output: 25
+}
