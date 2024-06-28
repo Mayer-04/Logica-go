f7fcf22bc907c77c77a3fb23b9989ab8ed24a217
Mayer-04 <andres.20.chaves@gmail.com>
2024-06-28 15:01:51 -0500

    nuevos ccomentarios y código

--- fundamentos/estructuras-control/for/for.go
+++ fundamentos/estructuras-control/for/for.go
@@ -4,59 +4,91 @@ import "fmt"
 
 func main() {
 
-	// TODO: En Go solo existe un tipo de bucle - FOR
+	//* En Go solo existe un tipo de bucle "for", el cual puede ser utilizado de diferentes maneras.
 
-	// Bucle for clasico
-	// TODO: inicialización; condición; actualización
+	//* Bucle for clásico
+	// Sintaxis: inicialización; condición; actualización
 	for i := 0; i <= 5; i++ {
-		fmt.Println("Bucle clasico", i)
+		fmt.Println("Bucle clásico", i)
 	}
 
-	// Bucle tipo while
+	//* Bucle tipo while
+	// Go no tiene un bucle while, pero se puede emular con for
 	i := 0
 	for i <= 3 {
-		fmt.Println("while", i)
+		fmt.Println("Bucle tipo while", i)
 		i++
 	}
 
-	// Bluce for con range
-	// TODO: Se utiliza para iterar colecciones como arrays, strings, slice, map, chan
+	//* Bucle for con range
+	// Se utiliza para iterar sobre colecciones como arrays, strings, slices, maps y canales
+	// Si solo necesitas el índice, puedes omitir el segundo valor.
+	// Si solo necesitas el valor, puedes usar el identificador blank _ para omitir el índice.
 	numeros := [4]int{1, 2, 3, 4}
+	// La variable i representa el índice y la variable v representa el valor
 	for i, v := range numeros {
-		fmt.Printf("Indice: %d, Valores: %v\n", i, v)
+		fmt.Printf("Índice: %d, Valor: %d\n", i, v)
 	}
 
-	// Bluce for con range de enteros
-	// TODO: Desde la versión 1.22 se puede utilizar range con un número entero como argumento
+	//* Bucle for con range de enteros
+	// Desde la versión 1.22 se puede utilizar range con un número entero como argumento
+	// Itera desde 0 hasta n-1
 	for i := range 3 {
-		fmt.Println(i)
+		fmt.Println(i) // Output: 0, 1, 2
 	}
 
-	// Bucle for con break
+	//* Bucle for con break
 	for i := 1; i <= 100; i++ {
-
+		// Cuando i sea igual a 50, rompe el bucle
 		if i == 50 {
 			break
 		}
+		// Imprime los números del 1 al 49
 		fmt.Println(i)
 	}
 
-	// Bucle for con continue
+	//* Bucle for con continue
 	for i := 0; i <= 5; i++ {
-		message := fmt.Sprintf("Soy el número %d", i)
+		// Cuando i sea igual a 4, imprime "Continuando..." y salta a la siguiente iteración
 		if i == 4 {
-			fmt.Println(message)
+			fmt.Println("Continuando...")
 			continue
 		}
+		// Imprime el mensaje para los valores de i que no son 4
+		message := fmt.Sprintf("Soy el número %d", i)
 		fmt.Println(message)
 	}
 
-	// Iterar un string
-	// TODO: Se debe hacer un casting a string() porque los caracteres son runas, nos retorna su valor Unicode
+	//* Iterar un string
+	// Se debe hacer un casting a string() porque los caracteres son runas, lo que nos retorna su valor Unicode
 	var nombre string = "Mayer"
-
 	for _, value := range nombre {
 		fmt.Println(string(value))
+	}
+
+	//* Bucle infinito
+	// Un bucle sin condiciones de salida explícitas se convierte en un bucle infinito
+	count := 0
+	for {
+		fmt.Println("Bucle infinito, cuenta:", count)
+		// Cuando count sea igual a 3, rompe el bucle
+		if count == 3 {
+			break // Salida del bucle infinito
+		}
+		count++
+	}
+
+	//* Bucles anidados para trabajar con una matriz (slice de slices)
+	matrix := [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+
+	for i := range matrix {
+		for j := range matrix[i] {
+			fmt.Printf("Elemento en [%d][%d] es %d\n", i, j, matrix[i][j])
+		}
 	}
 
 }
--- fundamentos/estructuras-control/if/if.go
+++ fundamentos/estructuras-control/if/if.go
@@ -2,30 +2,45 @@ package main
 
 import "fmt"
 
+//* En Go no existe el operador ternario.
+//* En Go no hay conversión automática a booleano
+//* La condición dentro de un if debe ser explícitamente una expresión booleana.
+//* No se permite asignación en la condición. Esto causará un error de compilación.
+
 func main() {
 
-	// TODO: La condición no debe estar entre paréntesis
-
-	// Estructura if - La condición debe ser verdadera para que se ejecute el bloque de código
+	//* Estructura if - La condición no debe estar entre paréntesis
+	// La condición debe ser verdadera para que se ejecute el bloque de código
 	if true {
 		fmt.Println("Hola Mundo!")
 	}
 
 	edad := 18
-	// Estructura if-else
-	// TODO: Si la edad es mayor o igual a 18, mostrar el mensaje "Eres mayor de edad". De lo contrario, mostrar el mensaje "Eres menor de edad".
+	//* Estructura if-else
+	// Si la edad es mayor o igual a 18, mostrara el mensaje "Eres mayor de edad"
+	// De lo contrario, mostrara el mensaje "Eres menor de edad"
 	if edad >= 18 {
 		fmt.Println("Eres mayor de edad")
 	} else {
 		fmt.Println("Eres menor de edad")
 	}
 
-	// If con declaración de variable corta
-	// TODO: El ambito de la variable corta es el bloque de código en el que se encuentra
+	//* If con declaración de variable corta
+	// El ámbito de la variable corta 'mayor' es el bloque de código en el que se encuentra
 	if mayor := edad >= 18; mayor {
 		fmt.Println("Eres mayor de edad")
 	} else {
 		fmt.Println("Eres menor de edad")
+	}
+
+	//* Estructura if-else
+	// Esta estructura permite evaluar múltiples condiciones
+	if edad < 13 {
+		fmt.Println("Eres un niño")
+	} else if edad < 18 {
+		fmt.Println("Eres un adolescente")
+	} else {
+		fmt.Println("Eres un adulto")
 	}
 
 }
