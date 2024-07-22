f16c2e4e2a99286b3fdce476ff1d9c015080a2fb
Mayer-04 <andres.20.chaves@gmail.com>
2024-07-22 13:57:53 -0500

    Cambios en los métodos de punteros

--- fundamentos/structs/metodos/metodos.go
+++ fundamentos/structs/metodos/metodos.go
@@ -10,11 +10,12 @@ type Product struct {
 
 // Definición del struct "Person"
 type Person struct {
-	Name string
-	Age  int
+	Name     string
+	Lastname string
+	Age      int
 }
 
-//* La función saludar es un método del struct Person.
+// * La función saludar es un método del struct Person.
 // El parámetro entre paréntesis antes del nombre de la función es el "receptor"
 // El receptor (p Person) hace referencia a una instancia de la struct Person
 // Los métodos con receptores de tipo valor (no puntero) trabajan con una copia del valor del receptor
@@ -22,7 +23,7 @@ func (p Person) saludar() string {
 	return "Hola" + " " + p.Name
 }
 
-//* Punteros en métodos
+// * Punteros en métodos
 // La función modificar es un método del struct Product
 // El receptor (p *Product) es un puntero a una instancia de Product
 // Los métodos con receptores de tipo puntero pueden modificar el valor del receptor
@@ -35,7 +36,7 @@ func main() {
 
 	// Antes de invocar métodos en una estructura, debes crear una instancia de esa estructura
 	// Esto se hace asignando valores a los campos del struct.
-	mayer := Person{Name: "Mayer", Age: 24}
+	mayer := Person{Name: "Mayer", Lastname: "Prada", Age: 24}
 	fmt.Println(mayer.saludar()) // Output: Hola Mayer
 
 	// Crear una instancia del struct Product e invocar su método modificar.
