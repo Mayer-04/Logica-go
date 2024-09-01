300fda8cb4d4ab92bb6a7c96b8b389c058b18c87
Mayer-04 <andres.20.chaves@gmail.com>
2024-08-31 22:11:23 -0500

    mejorando el código

--- fundamentos/context/context.go
+++ fundamentos/context/context.go
@@ -6,27 +6,43 @@ import (
 	"time"
 )
 
-// TODO: Pasar valores de una función a otra - De una goroutine a otra
-// TODO: WithDeadline: Fecha limite
+/*
+* Paquete context en Go
+El paquete `context` en Go es utilizadopara manejar la propagación
+de cancelaciones y plazos de tiempo (deadlines) en operaciones concurrentes.
 
-// * Context Propagation
-
-// Un contexto puede derivar de un contexto padre
-// TODO: Es buena practica cancelar los contextos para liberar los recursos del contexto
+* Context Propagation: Crear y propagar contextos.
+- Los contextos en Go son útiles para pasar valores entre funciones y para manejar cancelaciones
+en operaciones concurrentes, como en goroutines. Un contexto puede derivarse de otro contexto
+para propagar cancelaciones o fechas límite (deadlines).
+*/
 
 func main() {
-
-	// Crear un contexto vacio que no puede ser cancelado, contexto PADRE
+	// Crear un contexto de fondo (background) que es el contexto raíz (padre).
+	// Este contexto no puede ser cancelado y no tiene un plazo de tiempo asociado.
 	ctx := context.Background()
 
-	// Este contexto se deriva del contexto padre "ctx"
+	// Crear un contexto derivado de `ctx` que se puede cancelar.
+	// El contexto `ctxCancel` hereda el valor de `ctx` y puede ser cancelado
+	// para liberar recursos cuando ya no se necesita.
 	ctxCancel, cancel := context.WithCancel(ctx)
 
-	// Cuando la función main termine se ejecuta la función "cancel"
+	// `cancel()` se llama cuando la función `main` termine, para liberar los recursos asociados con `ctxCancel`.
+	// Es una buena práctica llamar a `cancel` para evitar fugas de recursos.
 	defer cancel()
 
-	ctxTimeout := context.WithTimeout(ctxCancel, time.Second)
+	// Crear un contexto derivado de `ctxCancel` con un tiempo de espera (timeout).
+	// El contexto `ctxTimeout` se cancelará automáticamente después de 1 segundo
+	// o si se llama a `cancel()`.
+	ctxTimeout, cancelTimeout := context.WithTimeout(ctxCancel, time.Second)
+	defer cancelTimeout() // Asegurar que los recursos de `ctxTimeout` también se liberen.
 
-	fmt.Println(ctxTimeout)
-
+	// Simular una operación que podría respetar el contexto de tiempo de espera.
+	select {
+	case <-time.After(3 * time.Second): // Simular una operación que dura 3 segundos.
+		fmt.Println("Operación completada.")
+	case <-ctxTimeout.Done():
+		// Se ejecuta si el contexto se cancela o se agota el tiempo.
+		fmt.Println("Operación cancelada o tiempo agotado:", ctxTimeout.Err())
+	}
 }
