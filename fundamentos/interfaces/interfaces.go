af8de445da3381d511b6eaa6dee1a8bb5bf3dddf
Mayer-04 <andres.20.chaves@gmail.com>
2024-08-31 22:12:18 -0500

    agregando implementación de la interfaz error

--- fundamentos/interfaces/interfaces.go
+++ fundamentos/interfaces/interfaces.go
@@ -8,7 +8,7 @@ Las interfaces en Go son una forma de definir un conjunto de métodos que un tip
 
 IMPORTANTE: Go utiliza un sistema de interfaces implícitas,
 lo que significa que no es necesario declarar que un tipo "implementa" una interfaz; simplemente,
-si un tipo posee todos los métodos definidos por una interfaz, entonces ese tipo satisface esa interfaz.
+si un tipo posee `todos` los métodos definidos por una interfaz, entonces ese tipo satisface esa interfaz.
 
 - Las interfaces pueden ser implementadas por cualquier tipo.
 - Un tipo puede implementar múltiples interfaces.
@@ -18,6 +18,12 @@ Esto se conoce como interfaces mínimas, lo que permite una mayor flexibilidad y
 como `Printer` en lugar de `Print`, siguiendo una convención del lenguaje.
 - Las interfaces pueden ser compuestas, es decir, una interfaz puede incluir otras interfaces.
 - IMPORTANTE: Puedes implementar interfaces definidas en otros paquetes de Go, no solo las que tú defines.
+
+* Ejemplos adicionales:
+- Una interfaz nula no tiene un valor ni un tipo concreto.
+Una interfaz nula se crea simplemente al declarar una interfaz sin asignarle un valor.
+- Una interfaz vacía tiene cero métodos y puede ser utilizada para manejar datos de cualquier tipo.
+Una interfaz vacía no es considerada una expresión.
 */
 
 // Printer es una interfaz mínima que define un solo método Print.
@@ -30,17 +36,32 @@ type Logger interface {
 	Log(message string)
 }
 
-// Person es una estructura que contiene el nombre y la edad de una persona.
-type Person struct {
-	Name string
-	Age  int
-}
-
 // PrinterLogger es una interfaz compuesta que incluye las interfaces Printer y Logger.
 // Cualquier tipo que implemente ambos métodos (Print y Log) satisfará la interfaz PrinterLogger.
 type PrinterLogger interface {
 	Printer
 	Logger
+}
+
+// MyError es una estructura personalizada que implementara la interfaz `error`.
+// Contiene un campo `Msg` que almacenará un mensaje de error.
+type MyError struct {
+	Msg string
+}
+
+// Error es un método que hace que MyError cumpla con la interfaz `error`.
+// La interfaz `error` es una interfaz nativa de Go que solo requiere la implementación
+// de un método: `Error() string`. Cualquier tipo que implemente este método puede
+// ser considerado un error en Go.
+// El método Error de MyError devuelve el mensaje de error almacenado en el campo `Msg`.
+func (e MyError) Error() string {
+	return e.Msg
+}
+
+// Person es una estructura que contiene el nombre y la edad de una persona.
+type Person struct {
+	Name string
+	Age  int
 }
 
 // Print es un método de Person que cumple con la interfaz `Printer`.
@@ -92,4 +113,42 @@ func main() {
 	}
 	person.Print()                 // Hola, mi nombre es Messi y tengo 37 años.
 	person.Log("Hola, soy Messi!") // [LOG]: Hola, soy Messi!
+
+	//* Creamos una instancia de MyError con un mensaje de error.
+	err := MyError{Msg: "Ocurrió un error"}
+
+	// Dado que MyError implementa el método Error, se puede asignar a una variable de tipo `error`.
+	var genericError error = err
+
+	// Imprimimos el mensaje de error.
+	fmt.Println("Error personalizado:", genericError.Error()) // Ocurrió un error
+
+	// Ejemplo de interfaz nula.
+	// No ha sido inicializada con ningún valor. Debido a esto, su valor es nil.
+	var i interface{}
+	fmt.Println("Interfaz nula:", i) // Interfaz nula: <nil>
+
+	// Esta interfaz vacía contiene en su interior un valor de tipo string.
+	e := interface{}("Go")
+	fmt.Println("Interfaz vacía con un valor de tipo string:", e) // Output: Go
+
+	// Ejemplo de interfaz vacía.
+	var emptyInterface interface{}
+	emptyInterface = 42
+	fmt.Println("Interfaz vacía con un int:", emptyInterface) // Interfaz vacía con un int: 42
+
+	emptyInterface = "Hello, World!"
+	fmt.Println("Interfaz vacía con un string:", emptyInterface) // Interfaz vacía con un string: Hello, World!
+
+	// Type assertion - Verificar el tipo de emptyInterface.
+	// Validar el tipo concreto que tiene la interfaz vacía y si es ese tipo obtener el valor que esta dentro de ella.
+	// Si emptyInterface.(type)
+	switch emptyInterface.(type) {
+	case int:
+		fmt.Println("Es un int")
+	case string:
+		fmt.Println("Es un string")
+	default:
+		fmt.Println("No es ni un int ni un string")
+	}
 }
