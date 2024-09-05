450bb2e240c3e3440482cf4a20760660d5350bd5
Mayer-04 <andres.20.chaves@gmail.com>
2024-09-04 23:03:10 -0500

    add: nueva solución ✅

--- ejercicios/ejercicio2/ejercicio2.go
+++ ejercicios/ejercicio2/ejercicio2.go
@@ -13,24 +13,43 @@ Por ejemplo, para el objeto { a: true, b: 42, c: false } la función debe retorn
 ya que son las dos propiedades que tienen valores booleanos.
 */
 
-type Map map[string]any
-
 func main() {
-	obj := Map{"a": true, "b": 42, "c": false}
-	result := propiedadesBooleanas(obj)
-	fmt.Println(result)
+	booleanProperties := map[string]any{
+		"a": true,
+		"b": 42,
+		"c": false,
+	}
+	result := booleanPropertyKeys(booleanProperties)
+	fmt.Printf("result: %v\n", result) // ["a", "c"]
+
+	// Solución 2
+	result2 := booleanPropertyKeys2(booleanProperties)
+	fmt.Printf("result2: %v", result2) // ["a", "c"]
 }
 
-func propiedadesBooleanas(obj map[string]any) []string {
-	var booleanProperties []string
+// Solución 1 utilizando generics ✅
+func booleanPropertyKeys[K string, V any](obj map[K]V) []K {
+	booleanKeys := make([]K, 0, len(obj))
 
 	for key, value := range obj {
-		typeof := reflect.TypeOf(value).Kind()
-
-		if typeof == reflect.Bool {
-			booleanProperties = append(booleanProperties, key)
+		if reflect.TypeOf(value).Kind() == reflect.Bool {
+			booleanKeys = append(booleanKeys, key)
 		}
 	}
 
-	return booleanProperties
+	return booleanKeys
+}
+
+// Solución 2 ✅
+func booleanPropertyKeys2(obj map[string]any) []string {
+	var booleanKeys []string
+
+	for key, value := range obj {
+		typeof := reflect.TypeOf(value).Kind()
+		if typeof == reflect.Bool {
+			booleanKeys = append(booleanKeys, key)
+		}
+	}
+
+	return booleanKeys
 }
